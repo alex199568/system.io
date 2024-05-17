f7e96a087de0214103181d4e6b49fe0c8a5a8980
Alexandru Bondarenco <a.bondarenco@maib.md>
2024-05-17 14:24:00 +0300

    Negative flag for rational

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,22 @@
 package io.system
 
 fun main() {
-    val r = Rational(-7, 6)
-    println(r)
+    val r1 = Rational(1, 2)
+    val r2 = Rational(-1, 2)
+    val r3 = Rational(1, -2)
+    val r4 = Rational(-1, -2)
+
+    val r5 = Rational(3, 2)
+    val r6 = Rational(-3, 2)
+    val r7 = Rational(3, -2)
+    val r8 = Rational(-3, -2)
+
+    println(r1)
+    println(r2)
+    println(r3)
+    println(r4)
+    println(r5)
+    println(r6)
+    println(r7)
+    println(r8)
 }
--- src/main/kotlin/Rational.kt
+++ src/main/kotlin/Rational.kt
@@ -7,6 +7,8 @@ data class Rational(
     private var denominator: Long
 ) {
 
+    private var negative = false
+
     constructor(
         nominator: Number = 0,
         denominator: Number = 1
@@ -17,26 +19,41 @@ data class Rational(
 
     init {
         require(denominator != 0L)
+
+        negative = if (nominator >= 0L && denominator >= 0L) {
+            false
+        } else if (nominator < 0L && denominator < 0L) {
+            false
+        } else {
+            true
+        }
+
+        nominator = abs(nominator)
+        denominator = abs(denominator)
+
         val gcd = gcd(nominator, denominator)
         nominator /= gcd
         denominator /= gcd
     }
 
     override fun toString(): String {
+        var result = ""
         val whole = nominator / denominator
-        if (whole == 0L) return "$nominator/$denominator"
-        val part = nominator - whole * denominator
-        if (nominator > 0L) return "$whole $part/$denominator"
-        return "-(${abs(whole)} ${abs(part)}/$denominator)"
+        if (whole == 0L) {
+            result = "$nominator/$denominator"
+        } else {
+            val part = nominator - whole * denominator
+            result = "$whole $part/$denominator"
+        }
+        if (negative) return "-($result)"
+        return result
     }
 
     companion object {
 
         private fun gcd(n: Long, d: Long): Long {
-            val absN = abs(n)
-            val absD = abs(d)
-            if (d == 0L) return absN
-            return gcd(absD, absN % absD)
+            if (d == 0L) return n
+            return gcd(d, n % d)
         }
     }
 }
