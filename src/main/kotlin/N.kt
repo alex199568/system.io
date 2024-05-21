c5eb510b4e636c154e07dcd9094048b8d96f3735
Alexandru Bondarenco <a.bondarenco@maib.md>
2024-05-21 16:47:05 +0300

    Even and odd sequencies

--- src/main/kotlin/N.kt
+++ src/main/kotlin/N.kt
@@ -7,3 +7,12 @@ import java.math.BigInteger
 fun Double.toBigInt(): BigInteger {
     return toLong().toBigInteger()
 }
+
+val evenSequence: Sequence<Rational> = generateSequence(Rational(2)) { it + 2 }
+val oddSequence: Sequence<Rational> = generateSequence(Rational(1)) { it + 2 }
+
+fun main() {
+    evenSequence.take(10).forEach { println(it) }
+    println("---")
+    oddSequence.take(10).forEach { println(it) }
+}
