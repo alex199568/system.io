bb7508ee5d41707848fd2d1ba6faffe3a270128f
Alexandru Bondarenco <a.bondarenco@maib.md>
2024-05-21 16:57:25 +0300

    Cube sequence

--- src/main/kotlin/N.kt
+++ src/main/kotlin/N.kt
@@ -12,7 +12,8 @@ val evenSequence: Sequence<Rational> = generateSequence(Rational(2)) { it + 2 }
 val oddSequence: Sequence<Rational> = generateSequence(Rational(1)) { it + 2 }
 val positiveSequence: Sequence<Rational> = generateSequence(Rational(0)) { it + 1 }
 val squareSequence: Sequence<Rational> = positiveSequence.map { it * it }
+val cubeSequence: Sequence<Rational> = positiveSequence.map { it * it * it }
 
 fun main() {
-    squareSequence.take(10).forEach { println(it) }
+    cubeSequence.take(10).forEach { println(it) }
 }
