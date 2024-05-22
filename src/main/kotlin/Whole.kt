f8bb81de5870081503fc80468520115f8bc89722
Alexandru Bondarenco <a.bondarenco@maib.md>
2024-05-22 13:46:07 +0300

    Whole obj methods

--- src/main/kotlin/Whole.kt
+++ src/main/kotlin/Whole.kt
@@ -22,12 +22,26 @@ class Whole(
         return value.toString()
     }
 
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Whole
+
+        return value == other.value
+    }
+
+    override fun hashCode(): Int {
+        return value.hashCode()
+    }
+
     companion object {
 
         @JvmStatic
         fun main(args: Array<String>) {
             val w = Whole(3)
-            println(w)
+            val w2 = Whole(3)
+            println(w == w2)
         }
     }
 }
