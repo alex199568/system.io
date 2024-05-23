f6393fc27ff2d5bae57e6dc52a8c8775de1b35aa
Alexandru Bondarenco <a.bondarenco@maib.md>
2024-05-23 15:43:06 +0300

    2d array for matrix

--- src/main/kotlin/Matrix.kt
+++ src/main/kotlin/Matrix.kt
@@ -2,49 +2,38 @@ package io.system
 
 import io.system.num.Rational
 
-class Matrix() {
+class Matrix(
+    private val n: Int,
+    private val m: Int
+) {
 
-    private val rows = mutableListOf<MutableList<Rational>>()
-
-    constructor(vararg rows: List<Rational>) : this() {
-        rows.forEach { row ->
-            this.rows += row.toMutableList()
+    private val elements = Array(n) {
+        Array(m) {
+            Rational(0)
         }
     }
 
     operator fun get(i: Int, j: Int): Rational {
-        if (i >= rows.size) return Rational(0)
-        val row = rows[i]
-        if (j >= row.size) return Rational(0)
-        return row[j]
+        return elements[i][j]
+    }
+
+    operator fun set(i: Int, j: Int, r: Rational) {
+        elements[i][j] = r
     }
 
     override fun toString(): String {
-        val longestRowLength = rows.maxOf { it.size }
         val sb = StringBuilder()
-        rows.forEachIndexed { i, row ->
-            sb.append("| ")
-            (0 until longestRowLength).forEach { j ->
-                sb.append(this[i, j])
+
+        for (i in 0 until n) {
+            for (j in 0 until m) {
+                val r = elements[i][j]
+                sb.append(r)
                 sb.append(" ")
             }
-            sb.append("|")
             sb.append("\n")
         }
+
         return sb.toString()
-    }
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as Matrix
-
-        return rows == other.rows
-    }
-
-    override fun hashCode(): Int {
-        return rows.hashCode()
     }
 
 
@@ -52,18 +41,10 @@ class Matrix() {
 
         @JvmStatic
         fun main(args: Array<String>) {
-            val m = Matrix(
-                listOf(Rational(1), Rational(2), Rational(1)),
-                listOf(Rational(0), Rational(3)),
-                listOf(Rational(4))
-            )
-            val m2 = Matrix(
-                listOf(Rational(1), Rational(2), Rational(1)),
-                listOf(Rational(0), Rational(3)),
-                listOf(Rational(4))
-            )
+            val m = Matrix(2, 3)
+            m[0, 0] = Rational(2)
+            m[1, 2] = Rational(3)
             println(m)
-            println(m == m2)
         }
     }
 }
